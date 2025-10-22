5deb5436ee2f0b5551b11c6add8c335b21130db5
rajnishprecogs <136425719+rajnishprecogs@users.noreply.github.com>
2025-10-22 09:46:36 +0100

    Implement structured and slow input checks
    Added input checks for structured and slow inputs with encryption functionality.
    
    Signed-off-by: rajnishprecogs <136425719+rajnishprecogs@users.noreply.github.com>

--- src/advanced_examples/explore_me.cpp
+++ src/advanced_examples/explore_me.cpp
@@ -46,3 +46,42 @@ static void trigger_double_free(){
     }
     buffer = 0;
 }
+
+#include <cstring>
+#include <zlib.h>
+#include <iostream>
+#include "explore_me.h"
+
+static long insecureEncrypt(long input);
+static void trigger_double_free();
+
+void ExploreStructuredInputChecks(InputStruct inputStruct){
+    if (inputStruct.c == "Attacker") {
+        if (insecureEncrypt(inputStruct.a) == 0x4e9e91e6677cfff3L) {
+            if (insecureEncrypt(inputStruct.b) == 0x4f8b9fb34431d9d3L) {
+                trigger_double_free();
+            }
+        }
+    }
+
+    return;
+}
+
+void ExploreSlowInputsChecks(int a, int b){
+    if (a == 48664131) {
+        for (int i = 0; i < b; i++) {
+            if (i % 100'000'000 == 0) {
+                std::cerr   << "In loop at position: " 
+                            << std::to_string(i) 
+                            << " of " 
+                            << std::to_string(b) 
+                            << std::endl;
+            }
+        }
+    }
+}
+
+static long insecureEncrypt(long input) {
+  long key = 0xefe4eb93215cb6b0L;
+  return input ^ key;
+}
